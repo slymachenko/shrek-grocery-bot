e59fc3f836a67f4555f98a70f611be532e0773ca
Senya <senya.s408@gmail.com>
2021-03-08 21:11:59 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const TelegramBot = require("node-telegram-bot-api");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const TOKEN = process.env.TOKEN;
+
+const bot = new TelegramBot(TOKEN, {
+  polling: {
+    interval: 300,
+    autoStart: true,
+    params: {
+      timeout: 10,
+    },
+  },
+});
+
+console.log("Bot have been started...");
+
+bot.onText(/^\/start$/, (msg) => {
+  const { id } = msg.chat;
+  const html = `
+  <strong>Oh, hello there!</strong>
+  <i>My name is Shrek(you may know me from my autobiographical films) and I'll help you deal with your grocery trip!</i>`;
+  bot.sendMessage(id, html, {
+    parse_mode: "HTML",
+    disable_notification: true,
+    reply_markup: {
+      keyboard: [["Edit", "View"]],
+    },
+  });
+});
