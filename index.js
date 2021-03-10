36502acc2502583caba5a48aad9df9a7b3686544
Senya <senya.s408@gmail.com>
2021-03-10 21:59:42 +0200

    view list functionality

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ const bot = new TelegramBot(TOKEN, {
 
 console.log("Bot have been started...");
 
+const list = [
+  "Apples",
+  "Bananas",
+  "Lemonade",
+  "Popcorn",
+  "Chips",
+  "Fish",
+  "Eggs",
+  "Potato",
+  "Salat",
+  "Watermelon",
+  "Cookies",
+  "Milk",
+];
+
 bot.onText(/^\/start$/, (msg) => {
   const { id } = msg.chat;
   const html = `
@@ -29,4 +44,33 @@ bot.onText(/^\/start$/, (msg) => {
       keyboard: [["Edit", "View"]],
     },
   });
+});
+
+bot.onText(/^View$/, (msg) => {
+  const { id } = msg.chat;
+
+  const arr = [];
+  list.forEach((el, index) => {
+    arr.push([{ text: el, callback_data: index }]);
+  });
+
+  const html = `
+  <strong>List</strong>`;
+  const options = {
+    parse_mode: "HTML",
+    disable_notification: true,
+    reply_markup: JSON.stringify({
+      inline_keyboard: arr,
+    }),
+  };
+
+  bot.sendMessage(id, html, options);
+});
+
+bot.onText(/^Edit$/, (msg) => {
+  const { id } = msg.chat;
+});
+
+bot.on("callback_query", (msg) => {
+  console.log(list[msg.data]);
 });
